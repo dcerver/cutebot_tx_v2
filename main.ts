f3087d3beca6ac3dd37ff455aa331f1cb6c3a1c2
dcerver <121509667+dcerver@users.noreply.github.com>
2023-04-23 17:44:03 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 radio.setGroup(1)
 basic.forever(function () {
     radio.sendValue("Yaw", Math.map(input.acceleration(Dimension.X), -1023, 1023, -100, 100))
-    radio.sendValue("Speed", Math.map(input.acceleration(Dimension.Y), -1023, 1023, 100, -100))
+    radio.sendValue("Speed", Math.map(input.acceleration(Dimension.Y), -1023, 1023, -100, 100))
 })
